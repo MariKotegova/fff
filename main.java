ad6c8f3117077c20bf1493804e4ff7836a41f265
Котегова Мария <m.kotegowa@yandex.ru>
2022-05-20 14:27:28 +0300

    Gogle

--- main.java
+++ main.java
@@ -6,7 +6,7 @@ public class main {
 
         map.put("Tanya", new UserInfo(16));
         map.put("Kolya", new UserInfo(55));
-
+        System.out.println(" rrrrrrrrrrrrrrrrrrrrrrrr");
         System.out.println(map.get("Tanya"));
        // System.out.println(map.get("Kolya"));
         map.put("Kolya", new UserInfo(66));
